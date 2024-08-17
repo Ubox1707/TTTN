7a0bc0219f4462d6a270ca3ef7985e820290512c
Ubox1707 <107450633+Ubox1707@users.noreply.github.com>
2024-08-17 22:04:48 +0700

    Delete src directory

--- src/Data/temptData.js
+++
@@ -1,8 +0,0 @@
-import avt from "../img/tempavt.jpg"
-
-export const Follower = [
-    {id:0, name: "Peter Parker", username: "Spider Man", img: avt},
-    {id:1, name: "Peter Pan", username: "Bad Guy", img: avt},
-    {id:2, name: "Iko Uwat", username: "The Raid", img: avt},
-    {id:3, name: "Timothe Chalamet", username: "Willy Wonka", img: avt}
-];
